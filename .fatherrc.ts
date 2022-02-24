cc01e0a043cdd5b1712f6fba7788a88912895846
wangxinyu <799639819@qq.com>
2022-02-24 14:25:37 +0800

    change code

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,16 @@
 export default {
   esm: 'rollup',
+  cjs: 'rollup',
   autoprefixer: {},
   cssModules: true,
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
 };
--- dist/Button/index.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-declare type IProps = {
-    children?: React.ReactNode;
-};
-declare const _default: (props: IProps) => JSX.Element;
-export default _default;
