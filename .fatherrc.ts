b756b4ef4887f17df6860471878b17414d78a66b
wangxinyu <799639819@qq.com>
2022-02-25 15:17:57 +0800

    change code

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,14 @@
 export default {
-  esm: 'rollup',
-  cjs: 'rollup',
+  esm: {
+    type: 'rollup',
+    file: 'index.esm',
+    // minify: true
+  },
+  cjs: {
+    type: 'rollup',
+    file: 'index.cjs',
+    // minify: true
+  },
   autoprefixer: {},
   cssModules: true,
   extraBabelPlugins: [
